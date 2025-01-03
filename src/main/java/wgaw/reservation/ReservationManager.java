c737560d16e9830454ddde4bfe71bd21a21667e6
W. Gawronski <wgawronski.wg@gmail.com>
2025-01-03 15:06:54 +0100

    add class ReservationManager

---
+++ src/main/java/wgaw/reservation/ReservationManager.java
@@ -0,0 +1,15 @@
+package wgaw.reservation;
+
+import java.time.LocalDateTime;
+
+public class ReservationManager {
+
+    public void createReservation(
+            Equipment equipment,
+            String customerName,
+            LocalDateTime startDate,
+            LocalDateTime endDate
+    ) {
+
+    }
+}

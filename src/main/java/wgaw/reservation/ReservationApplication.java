269b86ac9a281255bac0403fdb63fec8fc1c023a
W. Gawronski <wgawronski.wg@gmail.com>
2025-01-03 16:56:38 +0100

    ConsoleTest: Equpment

--- src/main/java/wgaw/reservation/ReservationApplication.java
+++ src/main/java/wgaw/reservation/ReservationApplication.java
@@ -3,11 +3,41 @@ package wgaw.reservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDateTime;
+
 @SpringBootApplication
 public class ReservationApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ReservationApplication.class, args);
+
+        // Create equipment using a factory
+        Equipment ski1 = EquipmentFactory.createEquipment(
+            EquipmentType.SKI,
+            "AA11",
+            "New",
+            "M"
+        );
+
+        Equipment ski2 = EquipmentFactory.createEquipment(
+            EquipmentType.SKI,
+                "BB22",
+                "Used, Scratched",
+                "Small"
+        );
+
+        Equipment snowboard1 = EquipmentFactory.createEquipment(
+            EquipmentType.SNOWBOARD,
+                "SS11",
+                "new",
+                "x-large"
+        );
+
+        System.out.println(ski1.getDetails());
+        System.out.println(ski2.getDetails());
+        System.out.println(snowboard1.getDetails());
+
     }
 
 }

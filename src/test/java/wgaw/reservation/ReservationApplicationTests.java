78358b6bc8770a47bdd3c83369e4fa797e731ce2
W. Gawronski <wgawronski.wg@gmail.com>
2025-01-03 14:05:24 +0100

    init2

--- src/test/java/wgaw/reservation/ReservationApplicationTests.java
+++ src/test/java/wgaw/reservation/ReservationApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ReservationApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

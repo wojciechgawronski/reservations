b39e44f47a1ab08de1a766c2af62b9f94bb38132
W. Gawronski <wgawronski.wg@gmail.com>
2025-01-03 18:24:57 +0100

    Reservations: active, completed, canceled

--- src/main/java/wgaw/reservation/Reservation.java
+++ src/main/java/wgaw/reservation/Reservation.java
@@ -3,11 +3,25 @@ package wgaw.reservation;
 import java.time.LocalDateTime;
 
 public class Reservation {
-    Equipment equipment;
-    String customerName;
+    private final Equipment equipment;
+    private final String customerName;
+    private final LocalDateTime startTime;
+    private final LocalDateTime endTime;
     private ReservationStatus status;
-    LocalDateTime startTime;
-    LocalDateTime endTime;
+
+    public Reservation(
+            Equipment equipment,
+            String customerName,
+            LocalDateTime startTime,
+            LocalDateTime endTime,
+            ReservationStatus status
+    ) {
+        this.equipment = equipment;
+        this.customerName = customerName;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.status = status;
+    }
 
     public ReservationStatus getStatus() {
         return status;
@@ -15,5 +29,30 @@ public class Reservation {
 
     public void setStatus(ReservationStatus status) {
         this.status = status;
+    }
+
+    public void getDetails() {
+        System.out.println("Reservation Details: Customer: " + customerName
+                + ", Start: " + startTime
+                + ", End: " + endTime
+                + ", Status: " + status
+                + ", Equipment: " + equipment.getEquipmentType() + ", SN: "+equipment.getSerialNumber()
+        );
+    }
+
+    public void cancel() {
+        this.status = ReservationStatus.CANCELLED;
+    }
+
+    public void complete() {
+        this.status = ReservationStatus.COMPLETED;
+    }
+
+    public boolean isCompleted(){
+        return this.status == ReservationStatus.COMPLETED;
+    }
+
+    public boolean isActive() {
+        return status == ReservationStatus.ACTIVE;
     }
 }
--- src/main/java/wgaw/reservation/ReservationApplication.java
+++ src/main/java/wgaw/reservation/ReservationApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class ReservationApplication {
@@ -38,6 +41,57 @@ public class ReservationApplication {
         System.out.println(ski2.getDetails());
         System.out.println(snowboard1.getDetails());
 
+        // reservation equipment on two days
+        LocalDateTime startTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime endTime = LocalDateTime.now().plusDays(3);
+        Reservation skiReservation1 = new Reservation(ski1, "john doe", startTime, endTime, ReservationStatus.ACTIVE );
+        Reservation snowboardReservation1 = new Reservation(snowboard1, "john doe", startTime, endTime, ReservationStatus.ACTIVE );
+        Reservation snowboardReservation2 = new Reservation(snowboard1, "jane doe", startTime, endTime, ReservationStatus.ACTIVE );
+
+        skiReservation1.getDetails();
+        snowboardReservation1.getDetails();
+
+        List<Reservation> reservations = new ArrayList<>();
+        reservations.add(skiReservation1);
+        reservations.add(snowboardReservation1);
+        reservations.add(snowboardReservation2);
+
+        // cancel one reservation
+        skiReservation1.cancel();
+
+        // show all reservations
+        System.out.println("All reservations:");
+        int index = 1;
+        for (Reservation reservation : reservations) {
+            System.out.println(index+".:");
+            reservation.getDetails();
+            index++;
+        }
+
+        // show active reservations
+        System.out.println("Active reservations:");
+        List<Reservation> reservationsActive = reservations.stream()
+                .filter(reservation -> reservation.getStatus() == ReservationStatus.ACTIVE)
+                .toList();
+
+        index = 1;
+        for (Reservation reservation : reservationsActive) {
+            System.out.println(index+".:");
+            reservation.getDetails();
+            index++;
+        }
+
+        // finalize reservation
+        snowboardReservation1.setStatus(ReservationStatus.COMPLETED);
+        System.out.println("Completed reservations:");
+        List<Reservation> reservationsCompleted = reservations.stream()
+                .filter(reservation -> reservation.getStatus() == ReservationStatus.COMPLETED)
+                .toList();
+        index = 1;
+        for (Reservation reservation : reservationsCompleted) {
+            System.out.println(index+".:");
+            reservation.getDetails();
+        }
     }
 
 }

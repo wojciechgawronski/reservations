3118f4a4bde94636b4897f4396289582e23a2df0
W. Gawronski <wgawronski.wg@gmail.com>
2025-01-03 16:55:13 +0100

    Add abstract class Equipmnet

--- src/main/java/wgaw/reservation/Equipment.java
+++ src/main/java/wgaw/reservation/Equipment.java
@@ -1,10 +1,27 @@
 package wgaw.reservation;
 
-abstract public class Equipment {
-    String serialNumber;
-    String conditions;
-    enum EquipmentType {}
-    EquipmentType equipmentType;
+public abstract class Equipment {
+    private final String serialNumber;
+    private final String conditions;
+    private final EquipmentType type;
+
+    public Equipment(String serialNumber, String conditions, EquipmentType type) {
+        this.serialNumber = serialNumber;
+        this.conditions = conditions;
+        this.type = type;
+    }
 
     abstract public String getDetails();
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getConditions() {
+        return conditions;
+    }
+
+    public EquipmentType getEquipmentType() {
+        return type;
+    }
 }

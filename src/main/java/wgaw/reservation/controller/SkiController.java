e055de99cc30006a8827f5b4d20bc0fe8aea313e
W. Gawronski <wgawronski.wg@gmail.com>
2025-01-04 20:34:42 +0100

    debug: ski

--- src/main/java/wgaw/reservation/controller/SkiController.java
+++ src/main/java/wgaw/reservation/controller/SkiController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import wgaw.reservation.model.Ski;
 import wgaw.reservation.repository.SkiRepository;
+import java.util.List;
 
 @Controller
 public class SkiController {
@@ -20,5 +23,11 @@ public class SkiController {
         var skis = skiRepository.findAll();
         model.addAttribute("ski", skis);
         return "ski";
+    }
+
+    @GetMapping("/ski/debug")
+    @ResponseBody
+    public List<Ski> debugSki() {
+        return skiRepository.findAll();
     }
 }
